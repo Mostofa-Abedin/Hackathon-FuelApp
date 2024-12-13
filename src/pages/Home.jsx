32868a407f3afc03ad48a1f401e737a0405a956d
Mostofa-Abedin <shekh.mostofa.abedin@gmail.com>
2024-12-13 14:46:02 +1100

    Some changes  for cleanup and slider component deleted

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,14 +9,10 @@ import Credits from "../components/Credits.jsx"; // Footer Component
 import LocateMe from '../components/LocateMe.jsx'; 
 // Importing the LocateMe component to display the user's current location.
 
-// import Layout from '../components/layout.jsx'; 
-// // Importing the Layout component to provide a consistent page structure with a Navbar and Footer.
-
 import MapComponent from '../components/MapComponent.jsx'; 
 // Importing the MapComponent to display an interactive map on the page.
 
-// import SliderComponent from '../components/SliderComponent.jsx'; 
-// // Importing the SliderComponent, which will allow users to adjust the search radius dynamically.
+
 
 import StationInfo from '../components/StationInfo.jsx'; 
 // Importing the StationInfo component to display details about petrol stations.
